54f0d4368d43d45056b65e011077037591c67a63
Ashutosh <ashutosh.bharadwaj99@gmail.com>
2023-06-24 15:31:33 +0530

    first commit

---
+++ 2-globals.js
@@ -0,0 +1,12 @@
+//GLOBALS - no window
+//__dirname : path to current directory
+//__filename : -file name
+// require - function to use modules(common js)
+//module - info about current module or file
+//process - info about env where program is executing
+
+console.log(__dirname)
+
+setInterval(()=> {
+    console.log("hello world")
+},1000)
